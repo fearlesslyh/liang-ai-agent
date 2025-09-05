d5b89ae119d829f2fc13a76482bd1171609c668e
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-09-05 19:56:44 +0800

    初始化

---
+++ src/main/java/com/lyh/liangaiagent/LiangAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lyh.liangaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiangAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiangAiAgentApplication.class, args);
+    }
+
+}
